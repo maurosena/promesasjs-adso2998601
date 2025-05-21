660541d6cab88c01d66b3c42d24a86f4c6112cee
maurosena <mvalenciazmisena@gmail.com>
2025-05-21 14:14:42 -0500

    Configuracion de promesas completo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
+
+//Funcion para simular la descarga de un archivo, utilizando promesas
+
+function descargarArchivo() {
+    return new Promise((resolve) => {
+        setTimeout(() =>{
+            resolve("📁 Archivo descargado con exito .... ")
+        }, 5000)
+    })
+}
+
 //Funcion con aync
-
-
-
 async function ejecutar(){
     try {
         console.log("⏳ Descarga en proceso..")
-        const respuesta = await 
+        const respuesta = await descargarArchivo()
+        console.log(respuesta)
     } catch (error) {
-        
+        console.error("✖️ Error al descargar", error)
     }
 }
-    
+  
+ejecutar()
     
