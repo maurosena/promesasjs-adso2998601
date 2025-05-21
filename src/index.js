a09ac050da13877e91280bd2cbac10080566e3db
maurosena <mvalenciazmisena@gmail.com>
2025-05-21 13:53:40 -0500

    Depuracion encarpetado del proyecto - Promesa con new Promise

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Funcion para simular la descarga de un archivo, utilizando promesas
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function descargarArchivo() {
+    return new Promise((resolve) => {
+        setTimeout(() =>{
+            resolve("📁 Archivo descargado con exito .... ")
+        }, 5000)
+    })
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(descargarArchivo()
+    .then((resultado) => {
+        console.log(resultado)
+    })
+)
+    
+    
